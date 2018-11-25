41a18c0208405cfba9d6dcac0c67ffe6d0e0f439
Joanna Tomassoni <jctomassoni@gmail.com>
2018-11-25 22:56:40 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,15 @@
+var chocolateBars = [
+  'snickers', 
+  'hundred grand', 
+  'kitkat', 
+  'skittles'
+  ];
+  
+  function addElementToBeginningOfArray (array, element) {
+    var myArray = ["original array item"];
+    myArray = [element, ...myArray];
+  }
+  
+  function destructivelyAddElementToBeginningOfArray (array, element) {
+    const
+  }
