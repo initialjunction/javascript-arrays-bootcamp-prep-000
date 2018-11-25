28e9373ab1d5dd48b1b203116940e7f6251d0826
Joanna Tomassoni <jctomassoni@gmail.com>
2018-11-25 23:59:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ var chocolateBars = [
   
   function addElementToBeginningOfArray(array, element) {
     const someArray = [1];
-    someArray.unshift("foo");
+    const newArray = someArray.unshift("foo");
   }
   
   function destructivelyAddElementToBeginningOfArray(array, element){
